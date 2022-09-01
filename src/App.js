b262abf100337b188bbac51bdab9a3f6ec31ed8b
di-marko <di-marko@hotmail.com>
2022-09-01 17:49:03 +0200

    Updated SCSS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.scss';
+import './App.js';
 import Header from './components/Header/Header';
 import Featured from './components/Featured/Featured';
 import Main from './components/Main/Main';

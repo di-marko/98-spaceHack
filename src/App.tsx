9f2757d4373071ad033f5902f3c63f3998a132ae
di-marko <di-marko@hotmail.com>
2022-09-02 13:24:19 +0200

    #23 Button link to repository added

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Header from './components/Header/Header';
 import Featured from './components/Featured/Featured';
 import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
+import GitButton from './components/GitButton/GitButton';
 
 function App() {
   return (
     <div className="app">
+      <GitButton />
       <Header />
       <Featured />
       <Main />
